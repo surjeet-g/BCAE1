9813104540880c5526b3973eedc7323118a456c8
v0647 <vipin.v@bahwancybertek.com>
2023-02-07 10:43:34 +0530

    android folder copied

---
+++ ios/Pods/Target Support Files/Pods-BCAE_App-BCAE_AppTests/Pods-BCAE_App-BCAE_AppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BCAE_App_BCAE_AppTests : NSObject
+@end
+@implementation PodsDummy_Pods_BCAE_App_BCAE_AppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BCAE_App/Pods-BCAE_App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BCAE_App : NSObject
+@end
+@implementation PodsDummy_Pods_BCAE_App
+@end
