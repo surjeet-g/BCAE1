ab6f5fa3df1c589a4d910fce7c42cf0a5dcebbdd
Surjeet Govinda Dash <surjeet.g@bahwancybertek.com>
2023-03-24 17:13:33 +0530

    pod removed

--- ios/Pods/Headers/Private/react-native-geocoder/RNGeocoder.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/@timwangdev/react-native-geocoder/ios/RNGeocoder.h
--- ios/Pods/Target Support Files/Pods-BCAE_App-BCAE_AppTests/Pods-BCAE_App-BCAE_AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BCAE_App_BCAE_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_BCAE_App_BCAE_AppTests
-@end
