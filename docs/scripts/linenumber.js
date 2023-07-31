7e23917d18e44e1800779476035cb4b5d994385c
v0647 <vipin.v@bahwancybertek.com>
2023-07-31 18:21:53 +0530

    merge with bug fix branch

---
+++ docs/scripts/linenumber.js
@@ -0,0 +1,30 @@
+/*global document */
+(() => {
+    const source = document.getElementsByClassName('prettyprint source linenums');
+    let i = 0;
+    let lineNumber = 0;
+    let lineId;
+    let lines;
+    let totalLines;
+    let anchorHash;
+
+    if (source && source[0]) {
+        anchorHash = document.location.hash.substring(1);
+        lines = source[0].getElementsByTagName('li');
+        totalLines = lines.length;
+
+        for (; i < totalLines; i++) {
+            lineNumber++;
+            lineId = `line${lineNumber}`;
+            lines[i].id = lineId;
+            if (lineId === anchorHash) {
+                lines[i].className += ' selected';
+            }
+        }
+    }
+    const d = document.getElementsByClassName('name')
+    console.log(d)
+    for (let i = 0; i < d.length; i++) {
+        d[i].innerText = d[i].innerText.replace("exports.", "")
+    }
+})();
